137be282cb203823a96a20ba8c3f3f1cf6f40af9
kenics <kenics@gmail.com>
2023-09-12 19:21:18 +0900

    feat: error occurs child_process

--- src/extensions/scratch3_dobotlite/index.js
+++ src/extensions/scratch3_dobotlite/index.js
@@ -3,7 +3,11 @@ const BlockType = require('../../extension-support/block-type');
 const Cast = require('../../util/cast');
 const log = require('../../util/log');
 
-const {PyhthonShell} = require('python-shell');
+const { spawn } = require('child_process');
+const pythonScriptPath = './my_script.py';
+const pythonArgs = ['arg1', 'arg2'];
+
+const pythonProcess = spawn('python', [pythonScriptPath, ...pythonArgs]);
 
 class Scratch3DobotLite {
     constructor (runtime) {
@@ -35,34 +39,21 @@ class Scratch3DobotLite {
     writeLog (args) {
         const text = Cast.toString(args.TEXT);
         log.log(text);
-	log.log(getStaticProps())
     }
-}
 
-export async function getStaticProps() {
-  // Pythonスクリプトを実行するためのオプションを設定
-  const options = {
-    scriptPath: './', // Pythonスクリプトのパスを指定
-    args: ['arg1', 'arg2'], // スクリプトに渡す引数を指定 (任意)
-  };
+    
 
-  // Pythonスクリプトを実行
-  const results = await new Promise((resolve, reject) => {
-    PythonShell.run('sample.py', options, (err, results) => {
-      if (err) {
-        reject(err);
+    pythonProcess.stdout.on('data', (data) => {
+      log.log('Python Script Output:' + {data});
+    });
+
+    pythonProcess.on('close', (code) => {
+      if (code === 0) {
+        log.log('Python Script Execution Successful');
       } else {
-        resolve(results);
+        log.log(`Python Script Execution Failed with Code: ${code}`);
       }
     });
-  });
-
-  // レスポンスを返す (例: Pythonスクリプトの実行結果をpropsとして渡す)
-  return {
-    props: {
-      pythonResults: results,
-    },
-  };
 }
 
 
