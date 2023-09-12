4c938ffd5950f96553844ae2da415e16fe678cec
kenics <kenics@gmail.com>
2023-09-12 18:57:20 +0900

    feat: error occur code

--- src/extensions/scratch3_dobotlite/index.js
+++ src/extensions/scratch3_dobotlite/index.js
@@ -3,6 +3,8 @@ const BlockType = require('../../extension-support/block-type');
 const Cast = require('../../util/cast');
 const log = require('../../util/log');
 
+const {PyhthonShell} = require('python-shell');
+
 class Scratch3DobotLite {
     constructor (runtime) {
         this.runtime = runtime;
@@ -33,7 +35,35 @@ class Scratch3DobotLite {
     writeLog (args) {
         const text = Cast.toString(args.TEXT);
         log.log(text);
+	log.log(getStaticProps())
     }
 }
+
+export async function getStaticProps() {
+  // Pythonスクリプトを実行するためのオプションを設定
+  const options = {
+    scriptPath: './', // Pythonスクリプトのパスを指定
+    args: ['arg1', 'arg2'], // スクリプトに渡す引数を指定 (任意)
+  };
+
+  // Pythonスクリプトを実行
+  const results = await new Promise((resolve, reject) => {
+    PythonShell.run('sample.py', options, (err, results) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(results);
+      }
+    });
+  });
+
+  // レスポンスを返す (例: Pythonスクリプトの実行結果をpropsとして渡す)
+  return {
+    props: {
+      pythonResults: results,
+    },
+  };
+}
+
 
 module.exports = Scratch3DobotLite;
---
+++ src/extensions/scratch3_dobotlite/my_script.py
@@ -0,0 +1,6 @@
+# my_script.py
+def main():
+    print("Hello from Python!")
+
+if __name__ == "__main__":
+    main()
