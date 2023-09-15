3146455a8ac6edf8823b414bb2db9f41fcb8bbde
kenics <kenics@gmail.com>
2023-09-15 17:08:21 +0900

    feat: get Web server response

--- src/extensions/scratch3_dobotlite/index.js
+++ src/extensions/scratch3_dobotlite/index.js
@@ -3,16 +3,13 @@ const BlockType = require('../../extension-support/block-type');
 const Cast = require('../../util/cast');
 const log = require('../../util/log');
 
-const { spawn } = require('child_process');
-const pythonScriptPath = './my_script.py';
-const pythonArgs = ['arg1', 'arg2'];
-
-const pythonProcess = spawn('python', [pythonScriptPath, ...pythonArgs]);
+const http = require('http');
 
 class Scratch3DobotLite {
     constructor (runtime) {
         this.runtime = runtime;
     }
+
 
     getInfo () {
         return {
@@ -39,22 +36,54 @@ class Scratch3DobotLite {
     writeLog (args) {
         const text = Cast.toString(args.TEXT);
         log.log(text);
+        httpClient.getHttpResponse();
     }
-
-    
-
-    pythonProcess.stdout.on('data', (data) => {
-      log.log('Python Script Output:' + {data});
-    });
-
-    pythonProcess.on('close', (code) => {
-      if (code === 0) {
-        log.log('Python Script Execution Successful');
-      } else {
-        log.log(`Python Script Execution Failed with Code: ${code}`);
-      }
-    });
 }
+
+class MyHTTPClient {
+  constructor() {
+    // コンストラクタで初期化などを行うことができます。
+  }
+
+  // HTTPリクエストを送信するメソッド
+  sendHttpRequest(callback) {
+    const options = {
+      hostname: 'localhost', // リクエストを送信するホスト名
+      port: 3000, // ポート番号（80はHTTPのデフォルト）
+      path: '/hello', // リクエストするパス
+      method: 'GET' // HTTPメソッド（GET、POSTなど）
+    };
+
+    // HTTPリクエストを作成
+    const req = http.request(options, (res) => {
+      // レスポンスデータを受信するコールバック
+      let data = '';
+
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      res.on('end', () => {
+        // レスポンスデータの受信が完了したときに実行
+        callback(data); // レスポンスデータをコールバック関数に渡す
+      });
+    });
+
+    // リクエストを送信
+    req.end();
+  }
+  
+  // レスポンスを取得するメソッド
+  getHttpResponse() {
+    this.sendHttpRequest((response) => {
+      // レスポンスデータを利用するコードをここに追加
+      console.log(response); // レスポンスデータをコンソールに出力など
+    });
+  }
+}
+
+// クラスのインスタンスを作成
+const httpClient = new MyHTTPClient();
 
 
 module.exports = Scratch3DobotLite;
